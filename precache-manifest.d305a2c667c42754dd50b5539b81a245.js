2537582a3b71ab1bb124d26cf35e513328d0fa98
Mark Wagdy <16P8146@eng.asu.edu.eg>
2020-07-29 17:27:58 +0200

    Updates

---
+++ precache-manifest.d305a2c667c42754dd50b5539b81a245.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa333410428858046ccd64512e19d7ce",
+    "url": "/publishedMyDoctor/index.html"
+  },
+  {
+    "revision": "740b54ef03a97cf8dccd",
+    "url": "/publishedMyDoctor/static/css/main.18e05f4c.chunk.css"
+  },
+  {
+    "revision": "08a3ba5b28aea9f51073",
+    "url": "/publishedMyDoctor/static/js/2.0292a1e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/publishedMyDoctor/static/js/2.0292a1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740b54ef03a97cf8dccd",
+    "url": "/publishedMyDoctor/static/js/main.016d6b75.chunk.js"
+  },
+  {
+    "revision": "d4281240b047b8486b85",
+    "url": "/publishedMyDoctor/static/js/runtime-main.ef9dad83.js"
+  },
+  {
+    "revision": "6ec90edb96a8078e9322dfa1f7a1c3ab",
+    "url": "/publishedMyDoctor/static/media/doctor (1).6ec90edb.svg"
+  },
+  {
+    "revision": "d2d84a1de4bce0fc6654db1f305bc8d3",
+    "url": "/publishedMyDoctor/static/media/healthcare-medical-concept-medicine-doctor-with-stethoscope-hand-patients-come_34200-313.d2d84a1d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/publishedMyDoctor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c486456879fdb92dfded8be38617ebd6",
+    "url": "/publishedMyDoctor/static/media/thumb-1920-513905.c4864568.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/publishedMyDoctor/precache-manifest.d305a2c667c42754dd50b5539b81a245.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/publishedMyDoctor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
